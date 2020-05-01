e6099fac04c6f9e5050ee473629270ee24d096a6
vinit <vinit.goradia@gmail.com>
2020-05-01 19:28:08 +0530

    Updates

---
+++ precache-manifest.bd8704be9a5d138d3f93b12eb5c3bb69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb743e61259449616b5859a52ecc333",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "6746bbd0a8343c4718fd",
+    "url": "/tictactoe/static/css/main.fb910d29.chunk.css"
+  },
+  {
+    "revision": "d67d2e7f2ea9ad4a0f36",
+    "url": "/tictactoe/static/js/2.93a4c239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.93a4c239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6746bbd0a8343c4718fd",
+    "url": "/tictactoe/static/js/main.7e314456.chunk.js"
+  },
+  {
+    "revision": "33d61b9d2c61db7b5436",
+    "url": "/tictactoe/static/js/runtime-main.37eed22a.js"
+  },
+  {
+    "revision": "bba201b1162f7df130d229345c488f44",
+    "url": "/tictactoe/static/media/icon_logo.bba201b1.jpg"
+  }
+]);
